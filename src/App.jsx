06aa27e93faa97fe114915066d95143d14bb1786
Sofi Rahmatulloh <118430785+tikdivmu@users.noreply.github.com>
2023-11-24 10:58:23 +0700

    tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function Square({ value, onSquareClick }) {
+
+  return <button className='square' onClick={onSquareClick}>
+    {value}
+    </button>;
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    console.log(nextSquares);
+    nextSquares[i] = 'X'; 
+    setSquares(nextSquares);
+  }
+
+  return (
+    <div className='board'>
+    <Square value = {squares[0]} onSquareClick = {() => handleClick(0)} />
+    <Square value = {squares[1]}/>
+    <Square value = {squares[2]}/>
+    <Square value = {squares[3]}/>
+    <Square value = {squares[4]}/>
+    <Square value = {squares[5]}/>
+    <Square value = {squares[6]}/>
+    <Square value = {squares[7]}/>
+    <Square value = {squares[8]}/>
+    </div>
+  );
+}
+
+
